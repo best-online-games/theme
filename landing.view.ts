c2b9e95f3fda6e357247c8da862415115be8a84a
novgorodcev.k <kirrallt@mail.ru>
2025-04-16 18:22:02 +0300

    start

---
+++ landing.view.ts
@@ -0,0 +1,30 @@
+namespace $ {
+	export class $horrorgamelanding extends $.$mol_page {
+		
+		@ $mol_mem
+		scroll_to_footer() {
+			const footer = this.foot()
+			if( footer && footer[0]?.dom_node() ) {
+				footer[0].dom_node()?.scrollIntoView({ behavior: 'smooth' })
+			}
+		}
+		
+		@ $mol_mem
+		submit_email() {
+			const email = this.email()
+			if( !email ) return
+			
+			// Here you would typically send the email to your backend
+			console.log( 'Email submitted:', email )
+			
+			// Clear the input
+			this.email( '' )
+		}
+		
+		@ $mol_mem
+		email( next?: string ) {
+			return next ?? ''
+		}
+		
+	}
+} 
