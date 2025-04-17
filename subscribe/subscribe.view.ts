9b7f2c9bcdadb317d85e305e4461922771a0b85c
novgorodcev.k <kirrallt@mail.ru>
2025-04-17 11:26:01 +0300

    ye

--- subscribe/subscribe.view.ts
+++ subscribe/subscribe.view.ts
@@ -1,42 +1,39 @@
 namespace $.$$ {
-
 	export class $horrorgamelanding_subscribe extends $.$horrorgamelanding_subscribe {
-
-		@ $mol_mem
+		@$mol_mem
 		domain() {
-			return this.$.$hyoo_crus_glob.home().hall_by( $horrorgamelanding_domain, {} )
+			return this.$.$hyoo_crus_glob.home().hall_by($horrorgamelanding_domain, {})
 		}
 
-		@ $mol_mem
-		email( next?: string ) {
+		@$mol_mem
+		email(next?: string) {
 			return next ?? ''
 		}
 
-		@ $mol_mem
-		message( next?: string ) {
+		@$mol_mem
+		message(next?: string) {
 			return next ?? ''
 		}
 
-		@ $mol_mem
-		submit( event?: Event ) {
-			if( !event ) return null
-			
+		@$mol_mem
+		submit(event?: Event) {
+			console.log(event)
+			if (!event) return null
+
 			const email = this.email()
-			if( !email ) {
-				this.message( 'Please enter your email' )
+			if (!email) {
+				this.message('Please enter your email')
 				return null
 			}
-
+			console.log(email)
 			const email_node = this.domain()!.email_make()
-			email_node.value( email )
-			email_node.date( new $mol_time_moment() )
+			email_node.value(email)
+			email_node.date(new $mol_time_moment())
 
-			this.message( 'Thank you for subscribing!' )
-			this.email( '' )
-			
+			this.message('Thank you for subscribing!')
+			this.email('')
+
 			return null
 		}
-
 	}
-
-} 
+}
