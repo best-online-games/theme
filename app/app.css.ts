c540990c70eb8f4b6d10ee031c07688d93e4176d
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-06-25 00:03:03 +0300

    работает ! э Э

--- app/app.css.ts
+++ app/app.css.ts
@@ -2,21 +2,23 @@ namespace $ {
 	const { per } = $mol_style_unit
 
 	$mol_style_define($horrorgamelanding_app, {
+		Logo: {
+			maxWidth: per(100),
+			maxHeight: per(100),
+			width: 'auto',
+			height: 'auto',
+			objectFit: 'contain',
+		},
 		Body_content: {
 			display: 'grid',
 			gridTemplateColumns: 'repeat(3, 1fr)',
-			maxWidth: per(1200),
-		},
-
-		'@media(max-width: 768px)': {
-			Body_content: {
-				gridTemplateColumns: 'repeat(2, 1fr)',
-			},
-		},
-
-		'@media(max-width: 480px)': {
-			Body_content: {
-				gridTemplateColumns: '1fr',
+			'@media': {
+				'(max-width: 480px)': {
+					gridTemplateColumns: 'repeat(1, 1fr)',
+				},
+				'(max-width: 768px)': {
+					gridTemplateColumns: 'repeat(2, 1fr)',
+				},
 			},
 		},
 	})
