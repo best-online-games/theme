447107f56f85dd3b6a3a92df095850711074743c
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-06-27 00:58:03 +0300

    ура

--- theme/demo/demo.view.ts
+++ theme/demo/demo.view.ts
@@ -1,15 +1,11 @@
 namespace $.$$ {
-	
-	export class $mol_theme_demo extends $.$mol_theme_demo {
-		
+	export class $bog_theme_demo extends $.$bog_theme_demo {
 		hue_deg() {
 			return this.hue() + 'deg'
 		}
-		
+
 		hue_spread_deg() {
 			return this.hue_spread() + 'deg'
 		}
-		
 	}
-	
 }
---
+++ theme/theme.ts
@@ -0,0 +1,21 @@
+namespace $ {
+	/**
+	 * Theme css variables
+	 * @see https://mol.hyoo.ru/#!section=demos/demo=mol_textarea_demo
+	 */
+	export const $bog_theme = $mol_style_prop('mol_theme', [
+		'back',
+		'hover',
+		'card',
+		'current',
+		'special',
+		'text',
+		'control',
+		'shade',
+		'line',
+		'focus',
+		'field',
+		'image',
+		'spirit',
+	] as const)
+}
