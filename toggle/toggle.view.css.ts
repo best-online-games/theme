4948bcd839269d4571eb80c4852baaaece0b8aa6
novgorodcev.k <novgorodcev.k@rwb.ru>
2025-11-03 20:50:59 +0300

    work some better on aroows finish 3 ( center )

---
+++ toggle/toggle.view.css.ts
@@ -0,0 +1,13 @@
+namespace $.$$ {
+    $mol_style_define($bog_theme_toggle, {
+        Bubble: {
+            position: 'fixed !important' as any,
+            left: '50% !important' as any,
+            top: '15vh !important' as any,
+            transform: 'translateX(-50%) !important' as any,
+            maxWidth: '400px',
+            width: '90vw',
+            maxHeight: '70vh',
+        },
+    })
+}
